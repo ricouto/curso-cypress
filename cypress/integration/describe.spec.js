001bcf65b5dd7ebe2a314dd37fc59bf7400969ef
ricouto <ricardocaputo@gmail.com>
2021-10-11 10:22:25 -0300

    Inclusao do describe

---
+++ cypress/integration/describe.spec.js
@@ -0,0 +1,25 @@
+///<reference types="cypress" />
+
+it.skip('A external test!', () => {
+    
+});
+
+describe('Should group tests...',() => {
+    describe('Should group more specfic tests...', () =>{
+        it('A specfic test #1!', () => {
+    
+        })
+    })
+
+    describe('Should group more specfic tests #2...', () =>{
+        it('A specfic test #2!', () => {
+    
+        })
+    })
+    
+    it('A internal test!', () => {
+    
+    })
+
+
+})
