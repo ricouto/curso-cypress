23552f6a782ffa26d597eb7b5e77758f23f72c5b
ricouto <ricardocaputo@gmail.com>
2021-10-11 21:26:58 -0300

    Uso do Pause e Debug no primeiro teste

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,11 +1,17 @@
 /// <reference types="cypress" />
 
 describe('Cypress basics', () => {
-    it('Should visit a page and assert title', () => {
+    it.only('Should visit a page and assert title', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
 
+        //cy.pause() consegue ir linha a linha e ver o fluxo da execucao
         cy.title().should('be.equal', 'Campo de Treinamento')
         cy.title().should('contain', 'Campo')
+        
+        /* me traz na Console mais informacoes
+        cy.title().should('contain', 'Campo').debug()
+        cy.title().debug.should('contain', 'Campo')
+        */
 
         cy.title()
             .should('be.equal', 'Campo de Treinamento')
@@ -13,5 +19,14 @@ describe('Cypress basics', () => {
 
         //TODO imprimir o log na Console
         //TODO escrever o title em um campo de texto  
+    });
+
+    it('Should find and interact with an element', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+        cy.get('#buttonSimple')
+            .click()
+            .should('have.value', 'Obrigado!')
+        
     });
 });
